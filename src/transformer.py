5c521f841b7614b192665eea4606039b43af7797
Dhruv Pai <46631243+dhruvbpai@users.noreply.github.com>
2022-08-25 20:23:37 +0100

    Update transformer.py

--- src/transformer.py
+++ src/transformer.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 import math
 import numpy as np
 
+
 class Transformer(nn.Module):
     def __init__(
         self,
@@ -18,7 +19,13 @@ class Transformer(nn.Module):
     ):
         super().__init__()
 
-        # Layers
+        self.model_type = "Transformer"
+        self.dim_model = dim_model
+
+        self.positional_encoder = PositionalEncoding(
+            dim_model=dim_model, dropout_p=dropout_p, max_len=5000
+        )
+        self.embedding = nn.Embedding(num_tokens, dim_model)
         self.transformer = nn.Transformer(
             d_model=dim_model,
             nhead=num_heads,
@@ -26,6 +33,26 @@ class Transformer(nn.Module):
             num_decoder_layers=num_decoder_layers,
             dropout=dropout_p,
         )
-
-    def forward(self):
-        pass
+        self.out = nn.Linear(dim_model, num_tokens)
+        
+    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+        src = src.permute(1,0,2)
+        tgt = tgt.permute(1,0,2)
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=tgt_pad_mask)
+        out = self.out(transformer_out)
+        
+        return out
+      
+    def get_tgt_mask(self, size) -> torch.tensor:
+        mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
+        return mask
+    
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        return (matrix == pad_token
