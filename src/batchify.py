f2b3d13c7cbccfe3749019dc20f5c87fd90309a9
Dhruv Pai <46631243+dhruvbpai@users.noreply.github.com>
2022-08-25 20:35:11 +0100

    Create batchify.py

---
+++ src/batchify.py
@@ -0,0 +1,22 @@
+def batchify_data(data, batch_size=16, padding=False, padding_token=-1):
+    batches = []
+    for idx in range(0, len(data), batch_size):
+        if idx + batch_size < len(data):
+            if padding:
+                max_batch_length = 0
+
+                # Get longest sentence in batch
+                for seq in data[idx : idx + batch_size]:
+                    if len(seq) > max_batch_length:
+                        max_batch_length = len(seq)
+
+                # Append X padding tokens until it reaches the max length
+                for seq_idx in range(batch_size):
+                    remaining_length = max_bath_length - len(data[idx + seq_idx])
+                    data[idx + seq_idx] += [padding_token] * remaining_length
+
+            batches.append(np.array(data[idx : idx + batch_size]).astype(np.int64))
+
+    print(f"{len(batches)} batches of size {batch_size}")
+
+    return batches
